a0d6464f4769408dd19707b953b729aadba7a4a2
Fernando <vadofernando9@gmail.com>
2026-04-22 12:58:10 +0200

    deploy final clientes

--- src/main/java/com/posicionup/crm/ClienteRepository.java
+++ src/main/java/com/posicionup/crm/ClienteRepository.java
@@ -1,14 +1,11 @@
 package com.posicionup.crm;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.List;
 
 public interface ClienteRepository extends JpaRepository<Cliente, Long> {
 
     List<Cliente> findByNombreContaining(String nombre);
-
     List<Cliente> findByEmailContaining(String email);
-
     List<Cliente> findByEmpresaContaining(String empresa);
 }
